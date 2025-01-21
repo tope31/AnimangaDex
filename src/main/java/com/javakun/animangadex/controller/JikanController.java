223e602e2b6f7d66f4eac8edad8862291e240d76
tope31 <christopherlim31@outlook.com>
2025-01-21 15:54:29 +0800

    hello world endpoint changes and test commit for author change

--- src/main/java/com/javakun/animangadex/controller/JikanController.java
+++ src/main/java/com/javakun/animangadex/controller/JikanController.java
@@ -1,12 +1,13 @@
 package com.javakun.animangadex.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("hello-world")
+@RequestMapping("/api")
 public class JikanController {
-
+    @GetMapping("/hello-world")
     public String helloWorld() {
         return "Hello World!";
     }
